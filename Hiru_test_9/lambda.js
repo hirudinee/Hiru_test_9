8d5ccfe571abc17a51208d41dddf95b86e9cd36a
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-22 14:23:07 +0530

    DB

--- Hiru_test_9/lambda.js
+++ Hiru_test_9/lambda.js
@@ -1,21 +1,20 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
-	s3.getBucketLocation({
-		'Bucket': "hiru.test"
-	}).promise()
-		.then(data => {
-			console.log(data);           // successful response
-			/*
-			data = {
-				LocationConstraint: "us-west-2"
-			}
-			*/
-		})
-		.catch(err => {
-			console.log(err, err.stack); // an error occurred
-		});
+	ddb.scan({
+		TableName: 'Hiru_table',
+		ExpressionAttributeValues: {},
+		FilterExpression: '',
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+
 
 	callback(null, 'Successfully executed');
 }
