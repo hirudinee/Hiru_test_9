f4c3d81623c1f51583fbbdc8f09a2686210a962a
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-26 10:32:41 +0530

    DB n SQS

--- Hiru_test_9/hiru.js
+++ Hiru_test_9/hiru.js
@@ -1,5 +1,18 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.put({
+		TableName: 'Test_Hiru',
+		Item: { 'ID': '001', 'Name': 'Hiru' }
+	}, function (err, data) {
+		if (err) {
+			console.log('error ', error);
+		} else {
+			console.log('success ', data);
+		}
+	});
 
-	callback(null,'Successfully executed');
+	
+
+	callback(null, 'Successfully executed');
 }
--- Hiru_test_9/thisisatest.js
+++ Hiru_test_9/thisisatest.js
@@ -1,8 +1,34 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.sendMessage({
+		MessageBody: 'This is a test message',
+		QueueUrl: 'https://sqs.us-east-2.amazonaws.com/263248768798/Hiru_SQS',
+		DelaySeconds: '0',
+		MessageAttributes: {
+			"001": {
+				"DataType": "String",
+				"StringValue": "test"
+			},
+			"002": {
+				"DataType": "Number",
+				"StringValue": "123"
+			},
+			"003": {
+				"DataType": "Binary",
+				"BinaryValue": "11"
+			}
+		}
+	}, function (data) {
+		console.log('Data ', data);
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+		console.log('error ', error);
+	});
 
 	//test
-	callback(null,'Successfully executed');
-	
-	
+	callback(null, 'Successfully executed');
+
+
 }
