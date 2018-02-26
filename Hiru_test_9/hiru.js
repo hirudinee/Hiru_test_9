08b080d6a5e8ecd2225e70e0e62cb4a681e66a32
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-26 10:39:48 +0530

    Added sort key

--- Hiru_test_9/hiru.js
+++ Hiru_test_9/hiru.js
@@ -3,7 +3,7 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	ddb.put({
 		TableName: 'Test_Hiru',
-		Item: { 'ID': '001', 'Name': 'Hiru' }
+		Item: { 'ID': '001', 'Sample': 'Hiru' }
 	}, function (err, data) {
 		if (err) {
 			console.log('error ', err);
